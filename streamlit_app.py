ee0c9d180255ff9931fd5fc294ffe6ea8768def7
gothchico <mahimaraut07@gmail.com>
2025-02-19 12:02:56 -0600

    debug public

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit as st
 def main():
     # builds the sidebar menu
     with st.sidebar:
-        st.page_link('demand-drivers.py', label='Demand Drivers', icon='📈')
+        st.page_link('demand_drivers.py', label='Demand Drivers', icon='📈')
         st.page_link('pages/burn.py', label='Token Burn', icon='🔥')
-        st.page_link('pages/fee-discounts.py', label='Fee Discounts', icon='💰')
+        st.page_link('pages/fee_discounts.py', label='Fee Discounts', icon='💰')
 
     st.title(f'📈 Demand Drivers')
 
