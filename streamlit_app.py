13beb9f50dbf773796b4dbb9513113f7040b4ae7
gothchico <mahimaraut07@gmail.com>
2025-03-05 16:12:30 -0600

    find demand drivers

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 def main():
     # builds the sidebar menu
     with st.sidebar:
-        st.page_link('Demand_Drivers.py', label='Demand Drivers', icon='📈')
+        st.page_link('pages/1_Demand_Drivers.py', label='Demand Drivers', icon='📈')
         st.page_link('pages/2_Burn.py', label='Token Burn', icon='🔥')
         st.page_link('pages/3_Fee_Discounts.py', label='Fee Discounts', icon='💰')
 
